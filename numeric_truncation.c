abde015fd8396eca522f7fc555df5dc7b0233fd8
Patricia Aas <psmaas@gmail.com>
2018-06-07 14:49:18 +0200

    Add some original examples

--- numeric_truncation.c
+++ numeric_truncation.c
@@ -1,3 +1,19 @@
-int main() {
+#include <limits.h>
+#include <string.h>
 
+/**
+ * https://cwe.mitre.org/data/definitions/197.html
+ */
+int main() {
+    unsigned int first_len = UINT_MAX - 256;
+    unsigned int second_len = 256;
+    unsigned int buf_len = 256;
+
+    char first[first_len], second[second_len], buf[buf_len];
+    int new_len = (first_len+second_len); // <- IDB (negative)
+
+    if(new_len <= 256) {
+        memcpy(buf, first, first_len);
+        memcpy(buf + first_len, second, second_len);
+    }
 }
--- stack_buffer_overflow.c
+++ stack_buffer_overflow.c
@@ -1,3 +1,11 @@
-int main() {
+#include <stdio.h>
 
+/**
+ * https://cwe.mitre.org/data/definitions/121.html
+ * https://cwe.mitre.org/data/definitions/242.html Use of Inherently Dangerous Function
+ */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+int main() {
+    char buffer[10];
+    gets(buffer); // <- Write outside
 }
