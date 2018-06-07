d68e081657d2843a260ab31eceb6a349630cc979
Patricia Aas <psmaas@gmail.com>
2018-06-07 15:51:28 +0200

    Add more of the original examples

--- disappearing_memset.c
+++ disappearing_memset.c
@@ -1,3 +1,20 @@
-int main() {
+#include <string.h>
+#include <stdbool.h>
 
+bool ConnectToMainframe(char *pwd) {
+  return 0;
+}
+
+bool GetPasswordFromUser(char *pwd, size_t pwd_size) {
+  return 0;
+}
+
+int main() {
+  char pwd[64];
+  if (GetPasswordFromUser(pwd, sizeof(pwd))) {
+    if (ConnectToMainframe(pwd)) {
+      // Interaction with mainframe
+    }
+  }
+  memset(pwd, 0, sizeof(pwd)); // <- Removed by the optimizer
 }
--- unnamed_lock_guard.cpp
+++ unnamed_lock_guard.cpp
@@ -1,3 +1,8 @@
-int main() {
+#include <mutex>
 
+int global_important_thing = 0;
+std::mutex global_mutex;  // protects g_i
+int main() {
+  std::lock_guard<std::mutex> lock(/*global_mutex*/);
+  ++global_important_thing;
 }
