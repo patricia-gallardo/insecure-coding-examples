866ac942f41a306c6d4dd472665623a791d8b98e
Patricia Aas <psmaas@gmail.com>
2018-06-07 15:59:59 +0200

    Whitespace

--- signed_integer_overflow.c
+++ signed_integer_overflow.c
@@ -6,14 +6,14 @@
  * https://cwe.mitre.org/data/definitions/190.html
  */
 int main() {
-    int first_len = INT_MAX;
-    int second_len = 256;
-    int buf_len = 256;
+  int first_len = INT_MAX;
+  int second_len = 256;
+  int buf_len = 256;
 
-    char first[first_len], second[second_len], buf[buf_len];
+  char first[first_len], second[second_len], buf[buf_len];
 
-    if((first_len + second_len) <= 256) { // <- UB (negative)
-        memcpy(buf, first, first_len);
-        memcpy(buf + first_len, second, second_len);
-    }
+  if((first_len + second_len) <= 256) { // <- UB (negative)
+    memcpy(buf, first, first_len);
+    memcpy(buf + first_len, second, second_len);
+  }
 }
