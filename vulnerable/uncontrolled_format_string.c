c6b633bc2992dd88c296a42500ef8d1d56f73774
Patricia Aas <psmaas@gmail.com>
2018-09-11 09:42:02 +0200

    Add link to exploit possibility (thanks Vesim)

--- vulnerable/uncontrolled_format_string.c
+++ vulnerable/uncontrolled_format_string.c
@@ -1,6 +1,8 @@
 /**
  * Use of Externally-Controlled Format String
  * https://cwe.mitre.org/data/definitions/134.html
+ *
+ * See exploit possibilities : https://www.exploit-db.com/docs/english/28476-linux-format-string-exploitation.pdf
  */
 
 #include <stdio.h>
