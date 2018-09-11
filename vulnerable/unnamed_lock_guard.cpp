d735ea15cb3f7ca061f54d2734e36c00f8791dc9
Patricia Aas <psmaas@gmail.com>
2018-09-11 09:27:13 +0200

    Fix unnamed lock_guard example (Thanks Vesim)

--- vulnerable/unnamed_lock_guard.cpp
+++ vulnerable/unnamed_lock_guard.cpp
@@ -1,5 +1,5 @@
 /**
- *
+ * http://clang.llvm.org/extra/clang-tidy/checks/bugprone-unused-raii.html
  */
 
 #include <mutex>
@@ -8,6 +8,6 @@ int global_important_thing = 0;
 std::mutex global_mutex;  // protects global_important_thing
 
 int main() {
-  std::lock_guard<std::mutex> lock(/*global_mutex*/);
+  std::lock_guard<std::mutex> /*lock*/{global_mutex};
   ++global_important_thing;
 }
