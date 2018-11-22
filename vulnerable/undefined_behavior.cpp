f5593bb2f6d17e73639ca8e8674a4a8d2a477937
Patricia Aas <psmaas@gmail.com>
2018-11-22 13:43:12 +0100

    Make a better name for the function

--- vulnerable/undefined_behavior.cpp
+++ vulnerable/undefined_behavior.cpp
@@ -7,12 +7,12 @@
 #include <limits.h>
 using namespace std;
 
-int foo(int x) {
+int isOnePlusXGreaterThanX(int x) {
  return (x + 1) > x;
 }
 
 int main() {
   cout << ((INT_MAX + 1) > INT_MAX) << "\n";
-  cout << foo(INT_MAX) << "\n";
+  cout << isOnePlusXGreaterThanX(INT_MAX) << "\n";
   return 0;
 }
