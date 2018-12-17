827dfa1e940a83ea2a60f11cca66da621612b910
Patricia Aas <psmaas@gmail.com>
2018-12-17 11:29:11 +0100

    Make it more similar to the original

--- format/getenvaddr.c
+++ format/getenvaddr.c
@@ -10,10 +10,16 @@
 
 int main(int argc, char **argv) {
 
-  if (argc < 2) {
-    printf("Usage: %s <environment var> \n", argv[0]);
+  if (argc < 3) {
+    printf("Usage: %s <environment var> <binary> \n", argv[0]);
     exit(0);
   }
 
-  printf("%s will be at %p\n", argv[1], getenv(argv[1]));
+  char *pointer = getenv(argv[1]);
+
+  printf("This name %s binary name %s\n", argv[0], argv[2]);
+  int length_difference =  (strlen(argv[0]) - strlen(argv[2]))*2;
+  printf("Length difference %d current pointer %p\n", length_difference, pointer);
+  pointer += length_difference;
+  printf("%s will be at %p\n", argv[1], pointer);
 }
