b11542eb9631478ff246da5f5d5b0867bf530567
Patricia Aas <psmaas@gmail.com>
2019-10-27 00:24:59 +0200

    Add example of if constexpr

---
+++ practice/constexpr_ub.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <limits>
+
+// Comment out to test
+#define constexpr
+
+int main() {
+  const int first_len = std::numeric_limits<int>::max();
+  const int second_len = 256;
+  const int buf_len = 256;
+
+  char first[first_len], second[second_len], buf[buf_len];
+
+  if constexpr ((first_len + second_len) <= 256) { // <- UB (negative)
+    memcpy(buf, first, first_len);
+    memcpy(buf + first_len, second, second_len);
+  }
+}
