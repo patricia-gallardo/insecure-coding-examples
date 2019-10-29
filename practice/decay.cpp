09f4eb9a01b513359dd2def2458784e138656f78
Patricia Aas <psmaas@gmail.com>
2019-10-29 19:59:55 +0100

    Add Array Pointer Decay and std::size example

---
+++ practice/decay.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void decay(const int* ages) {
+  // Size of the pointer = 8
+  std::cout << sizeof(ages) << '\n';
+  // Compile Error
+  //std::cout << std::size(ages) << '\n';
+}
+
+int main() {
+  int ages[] = { 15, 30, 60 };
+  // Number of elements = 3
+  std::cout << std::size(ages) << '\n';
+  // Size of an element = 4
+  std::cout << sizeof(ages[0]) << '\n';
+  // Size of array = 12 (= 3 * 4)
+  std::cout << sizeof(ages) << '\n';
+  decay(ages);
+}
