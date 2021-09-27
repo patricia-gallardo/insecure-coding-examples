04cde8ef2ee37a48255aeb93a9d5b9bb304734c5
Patricia Aas <psmaas@gmail.com>
2021-09-27 18:06:03 +0200

    Refactor filesystem.cpp example

--- practice/filesystem.cpp
+++ practice/filesystem.cpp
@@ -1,13 +1,15 @@
-#include <algorithm>
 #include <filesystem>
+#include <iostream>
 #include <string>
 
-std::string After(const std::string& user_input_pathname,
-                  const std::string& filename)
-{
-  using namespace std::filesystem;
-  path actual_path = canonical(user_input_pathname);
-  create_directories(actual_path);
+std::string make_path(const std::string & user_input_pathname,
+                      const std::string & filename) {
+  std::filesystem::path actual_path = std::filesystem::canonical(user_input_pathname);
+  std::filesystem::create_directories(actual_path);
   actual_path /= filename;
   return actual_path;
+}
+
+int main() {
+  std::cout << make_path("/tmp", "test_me.txt");
 }
