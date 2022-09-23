edd3bf765fa0e6373d24c375bf92ebdc3a8672ea
Patricia Aas <patricia@turtlesec.no>
2022-09-23 18:22:51 +0200

    Update to std::span

--- practice/guidelines/expressions_and_statements/cautious_pointer_use.cpp
+++ practice/guidelines/expressions_and_statements/cautious_pointer_use.cpp
@@ -1,11 +1,11 @@
-#include <gsl/span>
 #include <iostream>
+#include <span>
 
 /**
  * ES.42: Keep use of pointers simple and straightforward
  * https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#es42-keep-use-of-pointers-simple-and-straightforward
  */
-void process(gsl::span<const int> buffer) {
+void process(std::span<const int> buffer) {
   for (auto & x : buffer) {
     std::cout << x;
   }
