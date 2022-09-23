39e6572b867782e20a4a850dd0cd2550b156c3a1
Patricia Aas <patricia@turtlesec.no>
2022-09-23 19:08:15 +0200

    Add pre C++17 example

---
+++ practice/guidelines/functions/cautious_this_capture_cpp_pre17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/**
+ * F.54: If you capture this, capture all variables explicitly (no default capture)
+ * https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#f54-if-you-capture-this-capture-all-variables-explicitly-no-default-capture
+ */
+class Prefixer {
+public:
+  auto printer() {
+    return [prefix = this->prefix](std::string suffix) {
+      std::cout << prefix << " " << suffix << "!\n";
+    };
+  }
+  const std::string prefix = "Greetings,";
+};
+
+int main() {
+  Prefixer prefixer;
+  auto print = prefixer.printer();
+  print("Mortals");
+}
