25f6e03e27a394669857260206672da55a99f37b
Patricia Aas <patricia@turtlesec.no>
2022-09-23 17:52:30 +0200

    Reformat

--- practice/guidelines/expressions_and_statements/cautious_pointer_use_decay.cpp
+++ practice/guidelines/expressions_and_statements/cautious_pointer_use_decay.cpp
@@ -6,13 +6,19 @@
  * https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#es42-keep-use-of-pointers-simple-and-straightforward
  */
 void decay(const int * ages) {
-  std::cout << sizeof(ages) << '\n'; // Size of the pointer = 8
+  // Size of the pointer = 8
+  std::cout << sizeof(ages) << '\n';
+  // Compile Error
+  // std::cout << std::size(ages) << '\n';
 }
 
 int main() {
   int ages[] = { 15, 30, 60 };
-  std::cout << std::size(ages) << '\n'; // Number of elements = 3
-  std::cout << sizeof(ages[0]) << '\n'; // Size of an element = 4
-  std::cout << sizeof(ages) << '\n'; // Size of array = 12 (= 3 * 4)
+  // Number of elements = 3
+  std::cout << std::size(ages) << '\n';
+  // Size of an element = 4
+  std::cout << sizeof(ages[0]) << '\n';
+  // Size of array = 12 (= 3 * 4)
+  std::cout << sizeof(ages) << '\n';
   decay(ages);
 }
