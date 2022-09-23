86df08d15a0a1567fa68544d433bd9ef71fb8f05
Patricia Aas <patricia@turtlesec.no>
2022-09-23 18:56:44 +0200

    Fix for #1 make Spiderman polymorphic

--- practice/guidelines/expressions_and_statements/use_named_cast.cpp
+++ practice/guidelines/expressions_and_statements/use_named_cast.cpp
@@ -2,13 +2,18 @@
  * ES.49: If you must use a cast, use a named cast
  * https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#es49-if-you-must-use-a-cast-use-a-named-cast
  */
-class Spiderman {};
+struct Hero {
+  virtual ~Hero() = default;
+};
+class Spiderman : public Hero {};
 class Ironman {};
 
 int main() {
   Spiderman * peter = new Spiderman;
   Ironman * tony1 = (Ironman *)peter;
-  // Ironman * tony2 = static_cast<Ironman*>(peter); // Compile-time failure
+  // Compile-time failure
+  // Ironman * tony2 = static_cast<Ironman *>(peter);
   Ironman * tony3 = reinterpret_cast<Ironman *>(peter);
-  // Ironman * tony4 = dynamic_cast<Ironman*>(peter); // Run-time failure
+  // Run-time failure
+  Ironman * tony4 = dynamic_cast<Ironman *>(peter);
 }
